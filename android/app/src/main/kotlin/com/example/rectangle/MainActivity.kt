873420fb2a4ddd2f9fdb6077c408500c1314dc8c
nicolas <nicolas.lepage@mines-paristech.fr>
2021-10-06 10:01:11 +0200

    first commit in wrong directory name

---
+++ android/app/src/main/kotlin/com/example/rectangle/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rectangle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/category_route.dart
@@ -0,0 +1,90 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+import 'category.dart';
+
+final _backgroundColor = Colors.brown[50];
+
+/// Category Route (screen).
+///
+/// This is the 'home' screen of the Unit Converter. It shows a header and
+/// a list of [Categories].
+///
+/// While it is named CategoryRoute, a more apt name would be CategoryScreen,
+/// because it is responsible for the UI at the route's destination.
+class CategoryRoute extends StatelessWidget {
+  const CategoryRoute({Key? key}) : super(key: key);
+
+  static const _categoryNames = <String>[
+    'Length',
+    'Area',
+    'Volume',
+    'Mass',
+    'Time',
+    'Digital Storage',
+    'Energy',
+    'Currency',
+  ];
+
+  static const _baseColors = <Color>[
+    Colors.teal,
+    Colors.orange,
+    Colors.pinkAccent,
+    Colors.blueAccent,
+    Colors.yellow,
+    Colors.greenAccent,
+    Colors.purpleAccent,
+    Colors.red,
+  ];
+
+  /// Makes the correct number of rows for the list view.
+  ///
+  /// For portrait, we construct a [ListView] from the list of category widgets.
+  Widget _buildCategoryWidgets(List<Widget> categories) {
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) => categories[index],
+      itemCount: categories.length,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final categories = <Category>[];
+
+    for (var i = 0; i < _categoryNames.length; i++) {
+      categories.add(Category(
+        name: _categoryNames[i],
+        color: _baseColors[i] as ColorSwatch<dynamic>,
+        iconLocation: Icons.biotech,
+      ));
+    }
+
+    final listView = Container(
+      color: _backgroundColor,
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: _buildCategoryWidgets(categories),
+    );
+
+    final appBar = AppBar(
+        elevation: 0.0,
+        title: RichText(
+          text: const TextSpan(
+            text: 'Cross',
+            style: TextStyle(fontSize: 30, color: Colors.red, fontWeight: FontWeight.bold),
+            children: <TextSpan>[
+              TextSpan(text: '/', style: TextStyle(fontSize: 30, color: Colors.black, fontWeight: FontWeight.bold)),
+              TextSpan(text: 'Feed', style: TextStyle(fontSize: 30, color: Colors.black, fontWeight: FontWeight.bold)),
+            ],
+          ),
+        ),
+        backgroundColor: Colors.brown[50],);
+
+    return Scaffold(
+      appBar: appBar,
+      body: listView,
+    );
+  }
+}
